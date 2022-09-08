97c7133898b7a2430a4984478bfeca6b8faa47d5
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2022-09-08 09:47:22 +0200

    Add spellsitter

--- dot_config/nvim_mine/lua/plugins/auto/treesitter/spellsitter.lua
+++ dot_config/nvim_mine/lua/plugins/auto/treesitter/spellsitter.lua
@@ -1,4 +1,7 @@
 return {
   -- https://github.com/lewis6991/spellsitter.nvim
-  'lewis6991/spellsitter.nvim'
+  'lewis6991/spellsitter.nvim',
+  config = function()
+    require 'spellsitter'.setup {}
+  end
 }
